94572db78be385476d30d73d8313e92c7f45bfd5
koushickk <subtlehollow@gmail.com>
2020-08-07 07:25:34 +0530

    First commit

---
+++ registration.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Registration</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<?php
+
+require_once "db.php";
+
+if(isset($_REQUEST['username'])){
+$username = stripslashes($_REQUEST['username']);
+//escape special string
+$username = mysqli_real_escape_string($con,$username);
+//email
+$email = stripslashes($_REQUEST['email']);
+
+$email = mysqli_real_escape_string($con,$email);
+
+//password
+$password = stripslashes($_REQUEST['password']);
+
+$password = mysqli_real_escape_string($con,$password);
+
+$trn_date = date('y-m-d H:i:s');
+//query
+
+$query = "INSERT INTO `users`(username,password,emal,trn_date) VALUES('$username','".md5($password)."','$email','$trn_date')";
+$result = mysqli_query($con,$query);
+
+if($result){
+  echo "<div class='form'>
+<h3>You are registered successfully.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+
+}
+
+}else{
+?>
+<div class="form">
+<h1>Registration</h1>
+<form name="registration" action="" method="post">
+<input type="text" name="username" placeholder="Username" required />
+<input type="email" name="email" placeholder="Email" required />
+<input type="password" name="password" placeholder="Password" required />
+<input type="submit" name="submit" value="Register" />
+</form>
+</div>
+<?php } ?>
+</body>
+</html>
